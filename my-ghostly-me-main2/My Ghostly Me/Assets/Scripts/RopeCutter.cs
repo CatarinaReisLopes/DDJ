8058571afdf9718b9292d8dce424a3a74437b3c1
DianaMSJesus <83040508+DianaMSJesus@users.noreply.github.com>
2022-01-26 19:57:41 +0000

    Seringa completo
    Seringa já cai com os 2 botões

--- my-ghostly-me-main2/My Ghostly Me/Assets/Scripts/RopeCutter.cs
+++ my-ghostly-me-main2/My Ghostly Me/Assets/Scripts/RopeCutter.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class RopeCutter : MonoBehaviour
 {
+    public Collider2D butao2;
+    public Collider2D caixa;
+    
 
     private void OnTriggerEnter2D (Collider2D collider){
         GameObject objectToDestroy = GameObject.FindGameObjectWithTag("Link");
+
+        
+
         if(collider.GetComponent<CharacterController2D>() != null){
-            Destroy (objectToDestroy);
-            
+            if(butao2.IsTouching(caixa)){
+                Destroy (objectToDestroy);
+            }
         }
     }
 
